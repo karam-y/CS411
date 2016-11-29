bcd21ba2fa1c2463b896292a5b40305bbaf84583
andradej <andradej@bu.edu>
2016-11-28 21:46:46 -0500

    added request schema for mongo

--- NearBUY/app/models/request.js
+++ NearBUY/app/models/request.js
@@ -1,14 +1,22 @@
 // grab the mongoose module
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 // define our users model
 // module.exports allows us to pass this to other files when it is called
 var RequestSchema = new Schema({
-	buyer_id: String, //someone who requests the items
-	seller_id: String, //someone who is getting the items
-	price: String, //price of the items
-	time: String, //time of request
+	_id: String,
+	open: { type: Boolean, default: true},
+	items: [],
+	price: String,
+	notes: String,
+	buyer_id: String,
+	seller_id: String,
+	delivery_address: String,
+	delivery_city: String,
+	delivery_state: String,
+	delivery_zip: String
 });
 
-module.exports = mongoose.model('Users', RequestSchema);
+module.exports = mongoose.model('Requests', RequestSchema);
