cecf1490f5e41d37f428631ac7a4a2fa70747ca0
andradej <andradej@bu.edu>
2016-11-28 23:57:13 -0500

    added controller for find.html

--- NearBUY/public/js/controllers/FindCtrl.js
+++ NearBUY/public/js/controllers/FindCtrl.js
@@ -1,2 +1,41 @@
 angular.module('FindCtrl', []).controller('FindController', function($scope, $http, $q, $timeout) {
-}
+	$scope.city = "";
+	$scope.state = "";
+	$scope.zipcode = "";
+
+	$scope.submitCityState = function() {
+		$http({
+		  url: '/api/find_results/' + $scope.city + "/" + $scope.state, 
+		  method: "GET",
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+		    $scope.searchResults = response.data;
+		    console.log(response);
+		    console.log("successful GET");
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		    console.log(response);
+		    console.log("error in GET");
+		});
+	}
+
+	$scope.submitZipcode = function () {
+		$http({
+		  url: '/api/find_results/' + $scope.zipcode, 
+		  method: "GET",
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+		    $scope.searchResults = response.data;
+		    console.log(response);
+		    console.log("successful GET");
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		    console.log(response);
+		    console.log("error in GET");
+		});
+	}
+});
